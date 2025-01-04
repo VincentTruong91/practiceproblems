20dddefa88a725317f61b7ee372cc98e4db09747
VincentTruong91 <vincenttruong6050@gmail.com>
2025-01-04 10:27:08 -0800

    brute force

--- containsDuplicate.cpp
+++ containsDuplicate.cpp
@@ -1,3 +1,4 @@
+//Using hashset
 class Solution {
 public:
     bool hasDuplicate(vector<int>& nums) {
@@ -11,3 +12,19 @@ public:
         return false;
     }
 };
+
+//Brute force
+//Time Comp. -> O(n^2) bc uses 2 for loops nested
+//Space Comp. -> O(1) bc it doesn't initialize any arr, and only returns true/false
+class Solution {
+    bool hasDuplicate(vector<int>& nums) {
+        for(int i =0; i < nums.size(); i++){
+            for(int j = i + 1; j < nums.size(); j++){
+                if (nums[i] == nums[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
