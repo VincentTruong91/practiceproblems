8aa53f5213f0eb9a3a6cb60b5543aeb05fbbfdf2
VincentTruong91 <vincenttruong6050@gmail.com>
2025-01-17 06:51:31 -0800

    binary search time/space cp

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,3 +1,7 @@
+//binary search
+//time complexity -> O(log n) because of binary search
+//i.e worst case 12 -> 6 -> 3 -> 1
+//space complexity -> O(1) in memory because only used constant operations by variables
 class Solution{
 public:
     int binarySearch(vector<int> nums, int target){
