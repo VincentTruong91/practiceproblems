1f7bfb2f13d6184fe288d3e2d88819d205dfceb4
VincentTruong91 <vincenttruong6050@gmail.com>
2025-01-17 06:36:25 -0800

    binary search time/space complexity

--- binarySearch.py
+++ binarySearch.py
@@ -1,3 +1,7 @@
+#binary search
+#time complexity -> O(logn) used binary +while loop
+#(i.e. of binary 16 -> 8 -> 4 -> 2 -> 1 worse case)
+#space complexity -> O(1) only used a few extra variables like l, r, and m
 class Solution:
     def binarySearch(self, nums: List[int], target: int):
         l = 0
