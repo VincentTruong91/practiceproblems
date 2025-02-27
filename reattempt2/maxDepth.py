92de800b0b856cfd368f2b5a0cc636824eed5838
Vincent Truong <vincenttruong6050@gmail.com>
2025-02-27 14:05:46 -0800

    maxdepth using recursion and itterative dfs reattempt

---
+++ reattempt2/maxDepth.py
@@ -0,0 +1,34 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+# Recursive Solution DFS
+class RecursiveSolution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+    
+
+
+#ItterativeSolution DFS (using stack)
+class ItterativeSolution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        stack = [[root, 1]]
+        res = 0
+
+        #everytime we go through a level, we pop the node
+        while stack: 
+            node, depth = stack.pop() #[node, depth] from stack
+
+            # don't add to stack if node is null
+            if node != None:
+                res = max(res, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+
+        return res
