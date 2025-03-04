63d8b0a983c23699112b8ec7d06633c3ec26b28c
Vincent Truong <vincenttruong6050@gmail.com>
2025-03-03 22:19:00 -0800

    reattempt invertTree fix of itterative dfs and finish itterative bfs

--- invertTree.py
+++ invertTree.py
@@ -1,4 +1,6 @@
 #DFS to traverse binary tree
+#forgot to mention but this is preorder
+#because root, left, then right
 #time complexity -> O(n) for each node, DFS visit each node only once, which performs constant work done per node, time complexity is O(n)
 #space complexity -> O(n) due to using recursion, each recursive call adds a new frame to call stack. Height of tree determines depth of call stack. Worse case = skewed tree O(n), best case balanced tree O(log n), but space always consider worse case.
 class TreeNode:
@@ -26,9 +28,14 @@ class RecursiveDFSSolution:
 
 
 #uses stack for dfs itteration
-#for every level of the tree, pop the element(s)
-#from previous level to get children(s) (if any)
-#from the current level
+#this also uses pre-order traversal
+#nodes are processed in pre-order fashion
+#for example:
+#    1
+#   2 3
+#  4   5
+#
+# 1 -> 2 -> 4 -> 3 -> 5
 #time complexity -> O(n) due to itterating through all nodes in tree via while loop
 #space complexity -> O(n) due to using stack to apply equal work to all nodes in tree
 class ItterativeDFS:
@@ -48,4 +55,34 @@ class ItterativeDFS:
                 stack.append(node.right)
 
             
+        return root
+    
+
+
+#this solution 
+#goes throught each node from the previous level
+#and adds new children (if there are any) from the previous level
+#for this current node
+#Time complexity -> O(n) due to using a while loop to go through each node
+#space complexity -> O(n) due to applying equal work to all nodes for all nodes and
+# using a queue to store nodes
+from collections import deque
+class ItterativeBFSSolution:
+    def invertTree(self, root:Optional[TreeNode]) -> Optional[TreeNode]:
+        if root == None:
+            return None
+        
+        queue = deque([root]) #a list is passed, which is itteratble
+        #if i did queue = deque(root), it's not itterable, since it's a single node, error
+
+        while queue != None:
+            node = queue.popleft()
+
+            node.left, node.right = node.right, node.left
+
+            if node.left != None:
+                queue.append(node.left)
+            if node.right != None:
+                queue.append(node.right)
+        
         return root
