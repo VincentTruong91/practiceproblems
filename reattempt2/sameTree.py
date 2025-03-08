ed0df9de088005d071690beff3fe505e91d3b43f
Vincent Truong <vincenttruong6050@gmail.com>
2025-03-08 08:23:34 -0800

    attempt on itterative bfs solution for sameTree

--- reattempt2/sameTree.py
+++ reattempt2/sameTree.py
@@ -54,3 +54,50 @@ class ItterativeDFSSolution:
 #time complexity -> O(n) applied same work to all nodes in trees p and q
 #space complexity -> depends, O(logn) for balanced tree
 #but O(n) for unbalanced tree
+
+
+#BFS Solution:
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+from collections import deque #double ended queue
+class BFSSolution:
+    def sameTree(self, p:Optional[TreeNode], q:Optional[TreeNode]) -> bool:
+        queuep = deque([p])
+        queueq = deque([q])
+        
+        while queuep and queueq: #not empty
+            node1 = queuep.popleft()
+            node2 = queueq.popleft()
+            
+            if node1 == None and node2 == None:
+                return True
+                #this means that both left and right node of p and q tree
+                # doesn't have the node, basically identical
+
+            if node1 != None or node2 != None or (node1.val != node2.val):
+                return False
+                #this means that if node1 from p tree exists but node2 from q tree doesn't exist
+                # or the same vice versa, return False. Also take into account
+                #on if they both exist but their values are different
+
+            queuep.append(node1.left)
+            queuep.append(node1.right)
+
+            queueq.append(node2.left)
+            queueq.append(node2.right)
+
+            
+        #went through all of the nodes from both trees and haven't returned false,
+        #meaning that all nodes are identical from p and q, return True
+        return True
+
+
+#time complexity -> O(n) due to applying work to all nodes from p and q, where n represents num of nodes
+#space complexity -> O(n)
