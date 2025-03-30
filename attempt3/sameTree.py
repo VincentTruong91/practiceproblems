b3c307b64b8c4e046759d65e68689821a0fb3325
Vincent Truong <vincenttruong6050@gmail.com>
2025-03-29 23:52:11 -0700

    reattempt again on same tree

--- attempt3/sameTree.py
+++ attempt3/sameTree.py
@@ -20,3 +20,32 @@ class Solution:
 
         #first condition when false means that either p or q node doesn't exist when the other one does. second condition is checking if both tree nodes p and q have the same value, and if not, return False
         return False
+    
+
+    #Itterative DFS solution: pre-order traversal
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class ItterativeDFSSolution:
+    def sameTree(self, p:Optional[TreeNode], q:Optional[TreeNode]) -> bool:
+        stack = [(p,q)] #use tuple to avoid confusion and reduce overhead, more efficient
+
+        while stack: #when stack is not empty
+            node1,node2 = stack.pop()
+
+            if node1 == None and node2 == None:
+                continue #reached after leaf node
+
+            if node1 == None or node2 == None or(node1.val != node2.val):
+                return False
+                #this means if node1 from p doesn't exist while node2 from q exists
+                # or vice versa, or if they both exist,
+                #if their values don't match, return False
+
+            
+        return True
